41ed28adb7da98ed9509e425a6dc1fe4ce4b31e6
JaehyoYoon-dev <zzayo94@gmail.com>
2021-05-15 16:52:13 +0900

    u need to commit to push

--- 222.py
+++ 222.py
@@ -1,1 +1,12 @@
-print("hello")
+# i=list(range(2,10))
+# j=list(range(1,10))
+# for a in i:
+#     for b in j:
+#         print(a, " x ", b, " = ", a*b)
+
+# # 1~100 중 2의 배수 또는 11의 배수를 출력해라
+# a= list(range(2,101))
+# for x in a:
+#     if x%2==0 or x%11==0:
+#         print(x)
+
